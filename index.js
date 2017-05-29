c47d406f8647b2fa92594e2aa7c84ffc2d3ffbf3
Miguel Dorta <dortamiguel@gmail.com>
2017-05-29 18:15:28 +0100

    Fixed update method is slug name for field is not present

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Slug = require('slug')
 
-module.exports = function (schema, options) {
+module.exports = function(schema, options) {
 	const field = schema.tree.slug.slug
 
-	schema.pre('validate', function (next) {
+	schema.pre('validate', function(next) {
 		const _id = this['_id']
 		const slug = Slug(this[field])
 		const c = this.constructor
@@ -15,31 +15,33 @@ module.exports = function (schema, options) {
 		})
 	})
 
-	schema.pre('update', function (next) {
-		const _id = this._conditions['_id']
-		const slug = Slug(this._update.$set[field])
+	schema.pre('update', function(next) {
+		const data = this._update.$set
 
-		const that = this
-		this.model.slugify(false, slug, _id, 0, (final_slug) => {
-			that._update.$set['slug'] = final_slug
-			next()
-		})
+		if (data.hasOwnProperty(field)) {
+			const slug = Slug(data[field])
+			const _id = this._conditions['_id']
+
+			const that = this
+			this.model.slugify(false, slug, _id, 0, final_slug => {
+				data['slug'] = final_slug
+				next()
+			})
+		} else next()
 	})
 
 	// Unique slug generator, converts `slug` to `slug_1` if `slug` exists
-	schema.statics.slugify = function (c, slug, _id, count, cb) {
+	schema.statics.slugify = function(c, slug, _id, count, cb) {
 		// TODO: Check that slug is not bigger than 1000 characters
-		const final_slug = count ? `${ slug }_${ (count || '') }` : slug
+		const final_slug = count ? `${slug}_${count || ''}` : slug
 
 		const search_slug = item => {
 			if (item)
-			// Checks if current document has already the current slug
-				if (`${ item._id }` === `${ _id }`)
+				if (`${item._id}` === `${_id}`)
+					// Checks if current document has already the current slug
 					cb(final_slug)
-				else
-				return this.slugify(c, slug, _id, (count || 0) + 1, cb)
-			else
-			cb(final_slug)
+				else return this.slugify(c, slug, _id, (count || 0) + 1, cb)
+			else cb(final_slug)
 		}
 
 		const that = c || this
